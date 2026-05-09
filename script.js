e8f41bcd9b6e7a694d79306662d86cf8b7ef32b0
Vitor Silva Reis Barros <vitorsrbarros@gmail.com>
2026-05-08 21:04:27 -0300

    feat: add function to human choice
    Refactoring getComputerChoice() function

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+function getHumanChoice(){
+  let humanChoice = prompt(`Rock, Paper or Scissors?`).toLowerCase();
+
+  if (humanChoice === `rock`) {
+    console.log(`You choose Rock!`);
+    return `rock`;
+  } else if (humanChoice === `paper`) {
+    console.log(`You choose Paper!`);
+    return `paper`;
+  } else {
+    console.log(`You choose Scissors!`);
+    return `scissors`;
+  }
+}
+const humanSelection = getHumanChoice();
+
+
 function getComputerChoice(){
   let computerChoice = Math.random() 
 
   if (computerChoice < 0.34) {
-    return (`Rock`);
+    console.log(`CPU choose Rock!`);
+    return (`rock`);
   } else if 
     (computerChoice >= 0.34 && computerChoice < 0.67) {
-      return (`Paper`)
+      console.log(`CPU choose Paper!`);
+      return (`paper`)
     }
   else {
-    return (`Scissors`)
+    console.log(`CPU choose Scissors!`);
+    return (`scissors`)
   }
 }
+const computerSelection = getComputerChoice();
+
+
