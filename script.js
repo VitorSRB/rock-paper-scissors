02e12f66157453a18f4ee219b4b555305585244d
Vitor Silva Reis Barros <vitorsrbarros@gmail.com>
2026-05-13 15:45:50 -0300

    feat: add playGame and finalScore function; refactor scope of variables

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getHumanChoice(){
+let humanScore = 0; 
+let computerScore = 0;
+
+function getHumanChoice(){ 
   let humanPlayer = prompt(`Rock, Paper or Scissors?`).toLowerCase();
 
   if (humanPlayer === `rock`) {
@@ -12,10 +15,8 @@ function getHumanChoice(){
     return `scissors`;
   }
 }
-const humanChoice = getHumanChoice();
 
-
-function getComputerChoice(){
+function getComputerChoice(){ 
   let computerPlayer = Math.random() 
 
   if (computerPlayer < 0.34) {
@@ -31,12 +32,8 @@ function getComputerChoice(){
     return (`scissors`)
   }
 }
-const computerChoice = getComputerChoice();
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) { 
   if (humanChoice === computerChoice) {
     console.log(`It's a draw! Both choose ${humanChoice}.`);
   } else if (
@@ -51,5 +48,38 @@ function playRound(humanChoice, computerChoice) {
     computerScore++;
     console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
   }
+
+  console.log(`SCORE`)
+  console.log(`YOU: ${humanScore} CPU: ${computerScore}`);
 }  
-playRound(humanChoice, computerChoice)
+
+function playGame() { 
+  const computerChoice = getComputerChoice();
+
+  const humanChoice = getHumanChoice();
+  
+  playRound(humanChoice, computerChoice)
+    
+}
+
+function finalScore() { 
+  if (humanScore === computerScore) {
+    console.log(`FINAL SCORE: YOU ${humanScore} CPU: ${computerScore}`);
+    console.log(`This battle was a DRAW!`);
+  } else if (humanScore > computerScore) {
+    console.log(`FINAL SCORE: YOU ${humanScore} CPU: ${computerScore}`);
+    console.log(`YOU WIN!`);
+  } else {
+    console.log(`FINAL SCORE: YOU ${humanScore} CPU: ${computerScore}`);
+    console.log(`CPU wins!`);
+  }
+}
+
+
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
+
+finalScore()
